2777ad69deff617e4d140eac82498985e5ee9893
Blake Sanie <blake@sanie.com>
2022-03-29 22:44:08 -0400

    landing page only

--- beta.py
+++
@@ -1,7 +0,0 @@
-from random import shuffle
-
-emails = ["priechma@purdue.edu", "mitra.samantha5@gmail.com", "burkedd@miamioh.edu", "swrgardner3@gmail.com", "michael@stroev.com", "troy.bresch@gmail.com", "alex.sikorsky17@gmail.com", "ramvandermeer@gmail.com", "hachadtarek@gatech.edu", "andypascual@gmail.com", "dtmace2@gmail.com", "paula@stroeva.com", "sofie@stroeva.com", "daksh510@gmail.com", "nikhildesh22@gmail.com", "michael@sanie.com", "celine@sanie.com", "max@stroev.com", "jane@stroeva.com"]
-shuffle(emails)
-print(emails)
-for i in range(1,len(emails),2):
-    print("|{}|{}|".format(emails[i - 1], emails[i]))
--- pro.js
+++
@@ -1,5 +0,0 @@
-// $(".pro").each(function() {
-//   $(this).append(
-//     "<a class='proMarker invisible needsToRender' href='/features'>Pro</a>"
-//   );
-// });
